9c7a4137b9ca2ffa60b6a6e9a356d32822993544
vicim994 <72468847+vicim994@users.noreply.github.com>
2023-02-24 10:36:40 +0100

    Small buggfix

--- SW_Characters/src/Controls.tsx
+++ SW_Characters/src/Controls.tsx
@@ -100,7 +100,7 @@ export default function Controls(props: ControlsProps) {
               variant="fullWidth"
               sx={{ width: "100%" }}
             >
-                {['E1', 'E2', 'E3', 'E4','E5', 'E6', 'E7', 'ALL'].map((value, index) => {
+                {['E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'ALL'].map((value, index) => {
                     return (
                         <Tab
                             label={value}
